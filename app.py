2db6ed61470f609cb53ee4194ba9e9b10ea158f7
cindy-muniz <cindypm2006@gmail.com>
2026-01-24 18:14:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -118,6 +118,6 @@ def update_chart(lat, lon):
 # 5️⃣ Run App
 # ------------------------------
 if __name__ == "__main__":
-    # host=0.0.0.0 for external access (Render requirement)
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run(debug=True, host="0.0.0.0", port=8050)
+
 
